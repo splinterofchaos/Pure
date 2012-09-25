431da8633bb4ffe304804d6f74808531cff20c16
Scott Prager <splinterofchaos@gmail.com>
2012-09-25 15:30:00 -0400

    Use fold over maximum . map when applicable.
    fold is significantly more efficient.
    
    Signed-off-by: Scott Prager <splinterofchaos@gmail.com>

--- euler/euler.cpp
+++ euler/euler.cpp
@@ -103,19 +103,39 @@ bool palindrome( int x ) {
     return _palindrome( digits(x) );
 }
 
+template< class R, class S >
+R maybeMaximum( R r, const S& s ) {
+    return null(s) ? r : std::max( r, maximum(s) );
+}
+
+template< class X > struct Largest {
+    X x;
+
+    Largest( X x ) : x(move(x)) { }
+
+    Largest& operator= ( const X& y ) {
+        x = std::max( x, y );
+    }
+};
+
+template< class X > 
+Largest<X> largest( X x ) {
+    return Largest<X>(move(x));
+}
+
 void problem4() {
     cout << "The largest palindrome product of three digit numbers :"
          << flush;
-    cout << maximum ( 
-        map ( 
-            []( const IRange& r ) -> unsigned int {
-                auto ps = filter( palindrome, 
-                                  map( times(last(r)), init(r) ) );
-                return notNull(ps) ? maximum(ps) : 0;
-            },
-            // We remove the first three values: {} {100}, and {100,101}.
-            drop( 3, inits(enumerate(100,999)) ) 
-        )
+    cout << foldl ( 
+        []( unsigned int largest, const IRange& r ) -> unsigned int {
+            return maybeMaximum( largest, 
+                                 filter( palindrome, 
+                                         // Note: f ^ xs = map(f,xs).
+                                         times(last(r)) ^ init(r) ) );
+        },
+        0,
+        // We remove the first three values: {} {100}, and {100,101}.
+        drop( 3, inits(enumerate(100,999)) ) 
     ) << endl;
 }
 
@@ -147,17 +167,12 @@ void problem6() {
     constexpr unsigned int sqrOfSum = sum(N) * sum(N);
 
     using P = float(*)(float,float);
-    cout << sqrOfSum - (unsigned int)sum ( 
-        map( rclosure(P(pow), 2), N ) 
-    ) << endl;
+    cout << sqrOfSum - (unsigned int)sum( rclosure(P(pow),2) ^ N ) << endl;
 }
 
 void problem7() {
     cout << "The 1001'st prime number: " << flush;
-    cout << *next ( 
-        begin( primes ), 
-        10000 
-    ) << endl;
+    cout << *next( begin(primes), 10000 ) << endl;
 }
 
 int from_sym( char sym ) { return sym - '0'; }
@@ -167,10 +182,11 @@ void problem8() {
 
     const string nsStr = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
 
-    cout << maximum (
-        map ( 
-            []( const vector<int>& v ) { return product( take(5,v) ); },
-            tails( mapTo<vector>(from_sym,nsStr) )
+    cout << (
+        foldl ( 
+            []( int x, const vector<int>& v ) {
+                return std::max( x, product( take(5,v) ) );
+            }, 0, tails( mapTo<vector>(from_sym,nsStr) )
         ) 
     ) << endl;
 }
@@ -241,7 +257,7 @@ unsigned int take_dir_prod( Vec dir, Vec pos, size_t n, const Mat& m ) {
         return 1;
 
     for( ; n--; pos = pos+dir ) 
-        prod *= ( m[get_y(pos)][get_x(pos)] );
+        prod *= m[get_y(pos)][get_x(pos)];
 
     return prod;
 }
