d053f8ef1c890c0d8e32d2ef4cf472bdf3fd43d2
Scott Prager <splinterofchaos@gmail.com>
2012-10-08 20:38:32 -0400

    Learn on.
    Signed-off-by: Scott Prager <splinterofchaos@gmail.com>

--- Functional.h
+++ Functional.h
@@ -378,6 +378,24 @@ constexpr P pairCompose( F f, G g ) {
     return P( move(f), move(g) );
 }
 
+template< class Binary, class Unary >
+struct On {
+    Binary b;
+    Unary  u;
+
+    template< class X, class Y >
+    auto operator () ( X&& x, Y&& y )
+        -> decltype( b( u(declval<X>()), u(declval<Y>()) ) )
+    {
+        return b( u(std::forward<X>(x)), u(std::forward<Y>(y)) );
+    }
+};
+
+template< class B, class U >
+constexpr On<B,U> on( B b, U u ) {
+    return { move(b), move(u) }
+}
+
 template< class X > constexpr X inc( X x ) { return ++x; }
 template< class X > constexpr X dec( X x ) { return --x; }
 
